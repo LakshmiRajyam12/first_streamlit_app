f385c6f233c0281328ef9c4c05e3e634677d2be1
Lakshmi Rajyam <106172738+LakshmiRajyam12@users.noreply.github.com>
2022-05-24 22:30:20 +0530

    Update streamlit_app.py
    added the emoji link to try

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.title('My Mom's New Healthy Diner')
+streamlit.header('https://unicode.org/emoji/charts/emoji-list.html#1f973 Breakfast Favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
