86413bd28c639a3c6a496a932740331518632960
Lakshmi Rajyam <106172738+LakshmiRajyam12@users.noreply.github.com>
2022-05-24 22:36:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title('My Moms New Healthy Diner')
 streamlit.header('\N{flexed biceps} Breakfast Favourites\N{flexed biceps}')
 streamlit.text('  Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
